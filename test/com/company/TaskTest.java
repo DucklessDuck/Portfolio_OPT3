413fedbcd7f54e0c945f703b2656dc48e1e5d1a5
DucklessDuck <nguyenkevin39@gmail.com>
2021-05-26 18:20:38 +0200

    fixed some issues :):):)

--- test/com/company/TaskTest.java
+++ test/com/company/TaskTest.java
@@ -9,4 +9,18 @@ class TaskTest {
     Task taskTest;
 
 
+    @Test
+    public void checkTaskTest(){
+        taskTest = new Task("firstTestTask", 10);
+        assertFalse(taskTest.checkTask(taskTest));
+        taskTest.setTaskTime(0);
+        taskTest.setHoursPassed(2);
+        assertFalse(taskTest.checkTask(taskTest));
+        taskTest.setTaskTime(100);
+        taskTest.setHoursPassed(100);
+        assertTrue(taskTest.checkTask(taskTest));
+    }
+
+
+
 }
